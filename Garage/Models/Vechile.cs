8dbcbfe694b706969649177fa804a47afeac4f9d
geekz <jjohan91@hotmail.com>
2024-09-30 10:54:07 +0200

    Filestructure

---
+++ Garage/Models/Vechile.cs
@@ -0,0 +1,14 @@
+﻿using Garage.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Models
+{
+    internal class Vechile: IVechile
+    {
+
+    }
+}
